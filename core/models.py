61a049ebfa72d63815b8cc508cb5dee17f13a6f7
greed-d <deekshit.bti@gmail.com>
2025-01-09 16:12:36 +0545

    `feat(models)` : add models for tracking money
    - Create `IncomeTracker` and `ExpenseTracker` model to track amount
    added by user
    - Create `ExpenseCategory` to track what category user spent on

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+INCOME_AT = [("Bank", "Bank"), ("Cash", "Cash"), ("Wallet", "Wallet")]
+EXPENSE_FROM = [("Bank", "Bank"), ("Cash", "Cash"), ("Wallet", "Wallet")] 
+
+class ExpenseCategory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField(max_length = 20)
+
+class IncomeTracker(models.Model):
+    amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
+    source = models.CharField(max_length=50, choices=INCOME_AT)
+    reason = models.CharField(max_length=50)
+    remarks = models.TextField()
+    time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class ExpenseTracker(models.Model):
+    amount = models.DecimalField(max_digits=12, default=0, decimal_places=2)
+    source = models.CharField(max_length=50, choices=EXPENSE_FROM)
+    reason = models.CharField(max_length=50)
+    category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE, null=True, blank=True)
+    remarks = models.TextField()
+    time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['time']
