ef13bf33298f72820ac922729e27c06f92242260
greed-d <deekshit.bti@gmail.com>
2025-01-13 18:00:31 +0545

    `feat(yasg)` : add swaggerUI for django

--- drftracker/settings.py
+++ drftracker/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import dotenv
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -46,8 +47,16 @@ INSTALLED_APPS = [
     # 'theme',
     # 'django_browser_reload'
     'rest_framework',
+    'drf_yasg',
     'core'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'core.utils.JWTAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 # TAILWIND_APP_NAME = 'theme'
 #
--- drftracker/urls.py
+++ drftracker/urls.py
@@ -16,11 +16,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from core.views import show_forms
+from core.views import show_forms, auth
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('income/', show_forms.IncomeView.as_view(), name='income'),
+    path('auth/', auth.LoginView.as_view(), name='auth'),
+    path('refresh/', auth.RefreshTokenView.as_view(), name='refresh_token'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
     # path("__reload__/", include("django_browser_reload.urls")),
